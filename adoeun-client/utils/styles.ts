5c568a5ab27087d57018b7558fcadd4aabb121e7
kimdoeun <doviisdev.git@gmail.com>
2025-05-24 15:47:38 +0900

    set root styles

---
+++ adoeun-client/utils/styles.ts
@@ -0,0 +1,12 @@
+export const Common = {
+  color:{
+    primary:'#4a154b'
+  },
+
+  fontSize:{
+    title:'4.8rem',
+    subTitle:'1.8rem',
+    paragraph:'1.4rem',
+    small:'1.2rem',
+  },
+}  
